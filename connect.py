855620af4a117bb202cdaac382c9e3d2631e8dfd
liasul <136583892+liasul@users.noreply.github.com>
2023-06-14 15:30:46 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,1 +1,5 @@
-#how to connect
+from web3 import Web3
+
+binance_testnet_rpc_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+web3 = Web3(Web3.HTTPProvider(binance_testnet_rpc_url))
+print(f"Is connected: {web3.isConnected()}")  # Is connected: True
